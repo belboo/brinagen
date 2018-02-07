a78cd83049275ebe04368ef63d1ef4a5d363307e
Oleg <obrovko@gmail.com>
2018-02-07 19:46:21 +0200

    Initial setup.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='brinagen',
+      version='0.1',
+      description='Sabrina\'s SNP tools',
+      url='http://github.com/belboo/brinagen',
+      author='Oleg Brovko',
+      author_email='obrovko@gmail.com',
+      license='MIT',
+      packages=['brinagen'],
+      zip_safe=False)
